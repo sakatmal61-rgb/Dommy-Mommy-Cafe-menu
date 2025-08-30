14a788ba4632598298a898f0a2a5cefbc50a76b6
sakatmal61-rgb <sakatmal6.1@gmail.com>
2025-08-30 12:36:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-q
+async function loadHTML(id, url) {
+  const res = await fetch(url);
+  document.getElementById(id).innerHTML = await res.text();
+}
+
+loadHTML("header", "header.html");
+loadHTML("menu", "menu.html");
+loadHTML("secret", "secret-menu.html");
+loadHTML("footer", "footer.html");
+
+// Paste your existing JavaScript below this to handle menu items, modals, and password
